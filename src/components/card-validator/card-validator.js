447fe1b487dcf101981f749747de96a9d27ec65a
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-11-20 20:55:02 +0300

    save

--- src/components/card-validator/card-validator.js
+++ src/components/card-validator/card-validator.js
@@ -2,21 +2,33 @@ import "./card-validator.css";
 
 export default class CardValidator {
   constructor(element) {
-    this.element = element;
+    if (typeof element === "string") {
+      this.element = document.querySelector(element);
+    }
 
-    this.onClickValidateButton = this.onClickValidateButton.bind(this);
-  }
-
-  init() {
     this.input = this.element.querySelector(".input-text");
     this.button = this.element.querySelector(".validate-button");
     this.icons = this.element.querySelectorAll(".payment-icon__pic");
 
+    this.onClickValidateButton = this.onClickValidateButton.bind(this);
+
     this.button.addEventListener("click", this.onClickValidateButton);
   }
 
-  onClickValidateButton(btn) {
-    btn.preventDefault();
-    console.log(btn);
+  onClickValidateButton(e) {
+    e.preventDefault();
+
+    const cardNumber = this.input.value.replace(/\s/g, "");
+
+    if (!cardNumber) {
+      console.log("Введите номер карты");
+      return;
+    }
+  }
+
+  getCardType(cardNumber) {
+    const types = {
+      visa:
+    }
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
 import CardValidator from "../components/card-validator/card-validator";
 
-const cardValidatorWidget = document.querySelector(".widget-container");
-
-const cardValidator = new CardValidator(cardValidatorWidget);
-
-cardValidator.init();
+const cardValidator = new CardValidator(".widget-container");
